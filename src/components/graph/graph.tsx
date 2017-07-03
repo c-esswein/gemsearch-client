764127567f05016e24bc5a40e437b65929287dc8
Christian Esswein <c.esswein@gmail.com>
2017-07-03 21:13:11 +0200

    added raycaster for graph

--- src/components/graph/graph.tsx
+++ src/components/graph/graph.tsx
@@ -17,25 +17,57 @@ export interface Props {
  */
 export class Graph extends React.Component<Props, null> {
 
-  renderContainer: HTMLElement;
-  renderer: THREE.WebGLRenderer;
-  scene: THREE.Scene;
-  camera: THREE.PerspectiveCamera;
-  controls: THREE.TrackballControls;
-  mouse: {x: number, y: number};
-  INTERSECTED: boolean;
-  positions: Float32Array;
+  private renderContainer: HTMLElement;
+  private renderer: THREE.WebGLRenderer;
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private controls: THREE.TrackballControls;
+  private mouse: {x: number, y: number};
+  private INTERSECTED: boolean;
+  private positions: Float32Array;
+  private particles: THREE.Points;
+  private typeMapping: ItemType[];
+  private raycaster: THREE.Raycaster;
+
+  private shouldAnimate: boolean = false;
+
+  constructor(props) {
+    super(props);
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
 
   componentDidMount() {
+    window.addEventListener('resize', this.onWindowResize.bind(this), false);
+
     this.renderThreeScene(this.renderContainer);
 
     xFetch('/api/nodes')
       .then(response => response.json())
       .then(json => this.drawGraph(json));
+    /*this.drawGraph({
+      nodes: [
+        0, 0, 0, 
+        0, 1, 1, 
+        0, .5, .5,
+        1, .3, .3,
+      ],
+      typeMapping: ['track', 'track', 'track', 'artist', 'artist']
+    });
+
+    this.drawLines({
+      edges: [
+        0, 2,
+        1, 3,
+      ]
+    });*/
   }
 
   componentWillUnmount() {
+    this.shouldAnimate = false;
     this.renderer.dispose();
+
+    window.removeEventListener('resize', this.onWindowResize.bind(this), false);
   }
 
   private renderThreeScene(renderContainer: HTMLElement) {
@@ -51,28 +83,23 @@ export class Graph extends React.Component<Props, null> {
     this.controls.panSpeed = 0.8;
     this.controls.dynamicDampingFactor = 0.3;
     this.controls.addEventListener('change', this.renderGL.bind(this));
-
-
-/*
-    var cube = new THREE.Mesh(new THREE.CubeGeometry( 200, 200, 200 ), new THREE.MeshNormalMaterial() );
-    cube.position.y = 150;
-    this.scene.add(cube);*/
     
     this.renderer = new THREE.WebGLRenderer({ alpha: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.setRendererSize();
     renderContainer.appendChild(this.renderer.domElement);
     
-    // raycaster = new THREE.Raycaster();
+    this.raycaster = new THREE.Raycaster();
     this.mouse = new THREE.Vector2();
-
-    window.addEventListener('resize', this.onWindowResize.bind(this), false);
-    document.addEventListener('mousemove', this.onDocumentMouseMove.bind(this), false);
-    
-    this.animate();
   }
 
-  private drawGraph(data: {nodes: number[], typeMapping: ItemType[], graph: number[]}) {
+  /**
+   * Draws graph nodes.
+   * @param data json server response:
+   *    nodes: contains 3D position for each node (as flatted array)
+   *    typeMapping: contains type for each node
+   */
+  private drawGraph(data: {nodes: number[], typeMapping: ItemType[]}) {
     const PARTICLE_SIZE = 2;
 
     const positions = Float32Array.from(data.nodes);
@@ -99,10 +126,14 @@ export class Graph extends React.Component<Props, null> {
     geometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
 
     const pointMaterial = new THREE.PointsMaterial({vertexColors : THREE.VertexColors });
-    const particles = new THREE.Points(geometry, pointMaterial);
-    this.scene.add(particles);
+    this.particles = new THREE.Points(geometry, pointMaterial);
+    this.scene.add(this.particles);
 
     this.positions = positions;
+    this.typeMapping = data.typeMapping;
+
+    this.shouldAnimate = true;
+    this.animate();
     
     xFetch('/api/graph')
       .then(response => response.json())
@@ -110,38 +141,29 @@ export class Graph extends React.Component<Props, null> {
   }
 
   /**
-   * Draws lines between nodes
-   * @param lines Array of Pairs which represents connected nodes.
-   * @param nodes Node positions.
+   * Draws lines between nodes.
    */
   private drawLines(data: {edges: number[]}) {
-  /*
-    var positions = new Float32Array(lines.length * 3 * 2);
-
-    var copyPoint = (target: Float32Array, i: number, source: Float32Array, j: number) => {
-      target[i * 3 + 0] = source[j * 3 + 0];
-      target[i * 3 + 1] = source[j * 3 + 1];
-      target[i * 3 + 2] = source[j * 3 + 2];
-    };
-
-    lines.forEach((line: number[], i: number) => {
-      copyPoint(positions, i * 2 + 0, nodes, line[0]);
-      copyPoint(positions, i * 2 + 1, nodes, line[1]);
-    });*/
-
     const geometry = new THREE.BufferGeometry();
     geometry.addAttribute('position', new THREE.BufferAttribute(this.positions, 3));
     geometry.setIndex(new THREE.BufferAttribute(new Uint16Array(data.edges), 1));
 
     const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-    const line = new THREE.Line(geometry, material);
+    const line = new THREE.LineSegments(geometry, material);
     this.scene.add(line);
   }
   
-  private onDocumentMouseMove(event: React.MouseEvent<HTMLElement>) {
-    event.preventDefault();
-    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+  private handleMouseMove(event: React.MouseEvent<HTMLElement>) {
+    // calculate relative position
+    const elPos = event.currentTarget.getBoundingClientRect();
+    const clientX = event.clientX - elPos.left;
+    const clientY = event.clientY - elPos.top;
+    
+    const width = this.renderContainer.offsetWidth;
+    const height = this.renderContainer.offsetHeight;
+
+    this.mouse.x = (clientX / width) * 2 - 1;
+    this.mouse.y = - (clientY / height) * 2 + 1;
   }
 
   private onWindowResize() {
@@ -159,33 +181,22 @@ export class Graph extends React.Component<Props, null> {
   }
 
   private animate() {
-    requestAnimationFrame(this.animate.bind(this));
-    this.controls.update();
-    this.renderGL();
-    // stats.update();
+    if (this.shouldAnimate) {
+      this.controls.update();
+      this.renderGL();
+      requestAnimationFrame(this.animate.bind(this));
+    }
   }
 
   private renderGL() {
-    /*
-    particles.rotation.x += 0.0005;
-    particles.rotation.y += 0.001;
-    var geometry = particles.geometry;
-    var attributes = geometry.attributes;
-    raycaster.setFromCamera(mouse, camera);
-    var intersects = raycaster.intersectObject(particles);
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObject(this.particles);
+
     if (intersects.length > 0) {
-      if (this.INTERSECTED != intersects[ 0 ].index) {
-        attributes.size.array[ this.INTERSECTED ] = PARTICLE_SIZE;
-        this.INTERSECTED = intersects[ 0 ].index;
-        attributes.size.array[ this.INTERSECTED ] = PARTICLE_SIZE * 1.25;
-        attributes.size.needsUpdate = true;
-      }
-    } else if (this.INTERSECTED !== null) {
-      attributes.size.array[ this.INTERSECTED ] = PARTICLE_SIZE;
-      attributes.size.needsUpdate = true;
-      this.INTERSECTED = null;
+      const type = this.typeMapping[intersects[0].index];
+      console.log(type);
     }
-    */
+
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -201,7 +212,7 @@ export class Graph extends React.Component<Props, null> {
               </div>
             ))}
           </div>
-          <div className="graph__canvas" ref={(el) => { this.renderContainer = el; }} />
+          <div className="graph__canvas" onMouseMove={this.handleMouseMove} ref={(el) => { this.renderContainer = el; }} />
       </div>
    );
   }
